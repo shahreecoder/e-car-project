c9006363acf875d2793f19792b02bb8c13939517
shahreecoder <codershahree@gmail.com>
2022-02-23 12:29:10 +0500

    managerServicelistdialogDone

--- app/src/main/java/com/example/e_carservices/owner/manageservices.java
+++ app/src/main/java/com/example/e_carservices/owner/manageservices.java
@@ -3,9 +3,12 @@ package com.example.e_carservices.owner;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -31,19 +34,45 @@ public class manageservices extends AppCompatActivity {
     ListView listView;
     serviceadaptor serviceadaptor;
     modelservice modelservice;
-    public static ArrayList<modelservice> modelserviceArrayList=new ArrayList<>();
+    public static ArrayList<modelservice> modelserviceArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manageservices);
-        listView=findViewById(R.id.servicelist);
-        serviceadaptor=new serviceadaptor(manageservices.this,modelserviceArrayList);
+        listView = findViewById(R.id.servicelist);
+        serviceadaptor = new serviceadaptor(manageservices.this, modelserviceArrayList);
         listView.setAdapter(serviceadaptor);
-        ownerSession ownerSession=new ownerSession(this);
+        ownerSession ownerSession = new ownerSession(this);
         servicedata(ownerSession.owenid());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
+
+                //Toast.makeText(manageservices.this,modelserviceArrayList.get(i).getSname(),Toast.LENGTH_LONG).show();
+
+                AlertDialog.Builder bilder = new AlertDialog.Builder(view.getContext());
+                //ProgressDialog progressDialog=new ProgressDialog(view.getContext());
+                CharSequence[] dialogitem = {"View Service", "Edit Service", "Delete Service"};
+                bilder.setTitle("Service: " + modelserviceArrayList.get(index).getSname());
+                bilder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        switch (i) {
+                            case 0: {
+                                Toast.makeText(manageservices.this, modelserviceArrayList.get(index).getSid(), Toast.LENGTH_LONG).show();
+                                break;
+                            }
+                        }
+
+                    }
+                });
+                bilder.create().show();
+            }
+        });
 
     }
+
     public void servicedata(String name) {
         ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Please Wait...");
@@ -58,31 +87,32 @@ public class manageservices extends AppCompatActivity {
 
                 modelserviceArrayList.clear();
                 try {
-                    JSONObject jsonObject=new JSONObject(response);
-                    String success=jsonObject.getString("success");
-                    JSONArray jsonArray=jsonObject.getJSONArray("data");
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    JSONArray jsonArray = jsonObject.getJSONArray("data");
 
 //                    modelservice =new modelservice("1",jsonArray.getJSONObject(2).getString("Sname"),"111","sdsd","ssss");
 //                    modelserviceArrayList.add(modelservice);
 //                    serviceadaptor.notifyDataSetChanged();
-                        for(int i=0; i<jsonArray.length();i++){
+                    for (int i = 0; i < jsonArray.length(); i++) {
 
-                            //int id=jsonArray.getJSONObject(2).getInt("Sid");
-                            String sname=jsonArray.getJSONObject(i).getString("Sname");
-                            String sprice=jsonArray.getJSONObject(i).getString("Sprice");
-                            String sdisp=jsonArray.getJSONObject(i).getString("Sdisp");
-                            String simg=jsonArray.getJSONObject(i).getString("Simage");
+                        String sid=jsonArray.getJSONObject(i).getString("Sid");
+                        String sname = jsonArray.getJSONObject(i).getString("Sname");
+                        String sprice = jsonArray.getJSONObject(i).getString("Sprice");
+                        String sdisp = jsonArray.getJSONObject(i).getString("Sdisp");
+                        String simg = jsonArray.getJSONObject(i).getString("Simage");
 
-                            modelservice =new modelservice(String.valueOf(i+1),sname,"j","j",simg);
-                            modelserviceArrayList.add(modelservice);
-                            serviceadaptor.notifyDataSetChanged();
+                        //modelservice = new modelservice(String.valueOf(i + 1), sname, "j", "j", simg);
+                        modelservice = new modelservice(sid, sname, sprice, sdisp, simg);
+                        modelserviceArrayList.add(modelservice);
+                        serviceadaptor.notifyDataSetChanged();
 
                     }
-                    Toast.makeText(getBaseContext(),"test",Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getBaseContext(),"test",Toast.LENGTH_LONG).show();
 
                     progressDialog.dismiss();
                     // Toast.makeText(getBaseContext(),checkaddress+checkshop,Toast.LENGTH_LONG).show();
-                }catch (JSONException e){
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
